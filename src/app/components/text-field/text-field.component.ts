6930d9c764a11486b15eadbdd700cd5203566e34
JuliaClifford <69593248+JuliaClifford@users.noreply.github.com>
2021-07-23 00:19:44 +0300

    String analyze added

--- src/app/components/text-field/text-field.component.ts
+++ src/app/components/text-field/text-field.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-text-field',
   templateUrl: './text-field.component.html',
   styleUrls: ['./text-field.component.css']
 })
+
 export class TextFieldComponent implements OnInit {
-
   randomString:string = '';
+  text = document.getElementById('textField');
 
-  constructor() { }
+  constructor() {
+    this.getString();
+  }
 
   ngOnInit(): void {
-    this.getString();
+    const source = interval(3000);
+    source.subscribe(() => this.getString())
   }
 
  getString(){
@@ -20,9 +25,35 @@ export class TextFieldComponent implements OnInit {
     while (this.randomString.length < 5) {
       this.randomString = btoa(Math.random().toString()).substr(10, 5);
     }
+   this.whatStringCreated(this.randomString)
     return this.randomString;
   }
 
+  whatStringCreated(randomString:string){
+    if(this.randomString.includes('0')){
+      this.containZero();
+    }
+    if(this.randomString === randomString.split("").reverse().join("")){
+      this.isPalindrome();
+    }
+
+    if(this.randomString.match("^[0-9]*$")){
+      this.isNumericString();
+    }
+  }
+
+  containZero(): void{
+    // this.text.className = '';
+    console.log("0");
+  }
+
+  isPalindrome(): void{
+    console.log("revert");
+  }
+
+  isNumericString(): void{
+    console.log('123');
+  }
 }
 
 
