f6034523bbd3614ce608b08d30fc82f6f260293d
JuliaClifford <69593248+JuliaClifford@users.noreply.github.com>
2021-07-22 22:11:08 +0300

    Random string added

--- src/app/components/text-field/text-field.component.ts
+++ src/app/components/text-field/text-field.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextFieldComponent implements OnInit {
 
+  randomString:string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getString();
+  }
+
+ getString(){
+    this.randomString = '';
+    while (this.randomString.length < 5) {
+      this.randomString = btoa(Math.random().toString()).substr(10, 5);
+    }
+    return this.randomString;
   }
 
 }
+
+
